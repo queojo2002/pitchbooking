60d66f3b9199561507da644db2e9018bcd85bef6
hieuhoccode287 <hieubien811703@gmail.com>
2024-06-02 20:33:13 +0700

    1-

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,42 +1,45 @@
+import AddNewPitchesScreen from './admin/AddNewPitchesScreen';
+import AdminChatScreen from './admin/AdminChatScreen';
+import ChatScreen from './ChatScreen';
+import HomeScreen from './admin/HomeScreen';
 import LoginScreen from './guest/LoginScreen';
+import ManagePitchesScreen from './admin/ManagePitchesScreen';
+import OnboardingScreen from './OnboardingScreen';
+import PitchesDetailScreen from './admin/PitchesDetailScreen';
 import RegisterScreen from './guest/RegisterScreen';
 import ResetPasswordScreen from './guest/ResetPasswordScreen';
-import UserHomeScreen from './users/UserHomeScreen';
-import UserHistoryScreen from './users/UserHistoryScreen';
-import UserNotificationScreen from './users/UserNotificationScreen';
-import UserProfileScreen from './users/UserProfileScreen';
-import UserChatScreen from './users/UserChatScreen';
-import UserBookingScreen from './users/UserBookingScreen';
-import UserConfirmBooking from './users/UserConfirmBooking';
-import HomeScreen from './admin/HomeScreen';
 import SettingsScreen from './admin/SettingsScreen';
-import ManagePitchesScreen from './admin/ManagePitchesScreen';
-import ManagerUser from './admin/ManageUser';
-import AddNewPitchesScreen from './admin/AddNewPitchesScreen';
-import OnboardingScreen from './OnboardingScreen';
-import UserProfileEditScreen from './users/UserProfileEditScreen';
-import UserTermsAndConditionsScreen from './users/UserTermsAndConditionsScreen';
-import PitchesDetailScreen from './admin/PitchesDetailScreen';
 import UpdatePitchesScreen from './admin/UpdatePitchesScreen';
+import UserBookingScreen from './users/UserBookingScreen';
+import UserChatScreen from './users/UserChatScreen';
+import UserConfirmBooking from './users/UserConfirmBooking';
+import UserHistoryScreen from './users/UserHistoryScreen';
+import UserHomeScreen from './users/UserHomeScreen';
+import UserNotificationScreen from './users/UserNotificationScreen';
+import UserProfileEditScreen from './users/UserProfileEditScreen';
+import UserProfileScreen from './users/UserProfileScreen';
+import UserTermsAndConditionsScreen from './users/UserTermsAndConditionsScreen';
+
 export {
-    LoginScreen,
+    AddNewPitchesScreen,
+    AdminChatScreen,
+    ChatScreen,
+    HomeScreen,
+    ManagePitchesScreen,
+    OnboardingScreen,
+    PitchesDetailScreen,
     RegisterScreen,
     ResetPasswordScreen,
-    HomeScreen,
-    UserHomeScreen,
-    ManagePitchesScreen,
     SettingsScreen,
-    ManagerUser,
-    UserHistoryScreen,
-    UserNotificationScreen,
-    UserProfileScreen,
-    OnboardingScreen,
-    UserChatScreen,
-    AddNewPitchesScreen,
-    UserBookingScreen,
-    UserProfileEditScreen,
-    UserTermsAndConditionsScreen,
-    PitchesDetailScreen,
     UpdatePitchesScreen,
+    UserBookingScreen,
+    UserChatScreen,
     UserConfirmBooking,
+    UserHistoryScreen,
+    UserHomeScreen,
+    UserNotificationScreen,
+    UserProfileEditScreen,
+    UserProfileScreen,
+    UserTermsAndConditionsScreen,
+    LoginScreen,
 };
